24522a99870cebba1a0db6c2eace3a90a808a605
CavanhaMan <cavanhaman@hotmail.com>
2019-05-26 12:43:31 -0300

    Adicionando javascript basico e melhorias de layout

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function enviarMensagem() {
+	var nomeEnvio = document.getElementById("nome").value;
+	alert("Obrigado " + nomeEnvio + "! Sua mensagem foi enviada com sucesso! Entraremos em contato brevemente.");
+}
+
+function enviarOpiniao() {
+	var nomeEnvio = document.getElementById("nome").value;
+	alert("Obrigado! Sua opinião foi enviada com sucesso!");
+}
+
