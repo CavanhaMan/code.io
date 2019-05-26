0fb7aa2065fe65e95cc19350852de27381a152ed
CavanhaMan <cavanhaman@hotmail.com>
2019-05-26 18:50:53 -0300

    Arrumando script de envio de Mensagem e Opiniao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-function enviarMensagem() {
-	var nomeEnvio = document.getElementById("nome").value;
-	alert("Obrigado " + nomeEnvio + "! Sua mensagem foi enviada com sucesso! Entraremos em contato brevemente.");
+window.onload = function(){
+	document.forms["enviaMensagem"].onsubmit = function() {
+		var nome = document.getElementById("nome").value;
+		alert("Obrigado " + nome + "! Sua mensagem foi enviada com sucesso! Entraremos em contato brevemente.");
+	}
+	document.forms["enviaOpiniao"].onsubmit = function(){
+		alert("Obrigado! Sua opinião foi enviada com sucesso!");
+	}
 }
-
-function enviarOpiniao() {
-	var nomeEnvio = document.getElementById("nome").value;
-	alert("Obrigado! Sua opinião foi enviada com sucesso!");
-}
-
